d1098f7534e9377bdeaf63bec43f9b9e96c57976
Lura Skye <fuyukaii@proton.me>
2024-07-03 21:41:04 +0100

    update ruff + reformat

--- src/serena/frameparser.py
+++ src/serena/frameparser.py
@@ -197,9 +197,7 @@ class FrameParser:
 
             # packet finished, construct frame from saved values
             self._processing_partial_packet = False
-            frame = self._make_frame(
-                self._saved_type, self._saved_channel, bytes(body)
-            )
+            frame = self._make_frame(self._saved_type, self._saved_channel, bytes(body))
             self._last_packet_buffer = bytearray()
 
             return frame
